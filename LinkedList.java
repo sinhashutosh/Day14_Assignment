0664d779ce772e3578a135982167078430420d0a
Ashutosh <ashu.cse.sln@gmail.com>
2022-10-01 17:10:10 +0530

    UC4 completed

--- LinkedList.java
+++ LinkedList.java
@@ -4,9 +4,11 @@ public class LinkedList {
     public static void main(String[] args) {
         LinkedListOperation list = new LinkedListOperation();
         list.append(56);
-        list.append(30);
         list.append(70);
         list.printList();
-
+        System.out.println();
+        System.out.println("************************************");
+        list.insertAfter(list.head,56,30);
+        list.printList();
     }
 }
--- LinkedListOperation.java
+++ LinkedListOperation.java
@@ -22,6 +22,19 @@ public class LinkedListOperation {
         head = new_node;
     }
 
+    // Inserts a new node after the given prev_node.
+    public void insertAfter(Node head, int prev_data, int new_Data) {
+        Node temp = head;
+        Node new_Node = new Node(new_Data);
+        while (temp.data != prev_data && temp != null) {
+            temp = temp.next;
+        }
+        if (temp.data == prev_data) {
+            new_Node.next = temp.next;
+            temp.next = new_Node;
+        }
+    }
+
     // Appends a new node at the end.
     public void append(int new_data) {
         Node new_node = new Node(new_data);
