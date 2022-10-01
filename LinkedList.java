6e7e5e575d52b7b103774604cfe046e750115d97
Ashutosh <ashu.cse.sln@gmail.com>
2022-10-01 08:16:28 +0530

    UC1 comleted

--- LinkedList.java
+++ LinkedList.java
@@ -3,9 +3,9 @@ package org.example;
 public class LinkedList {
     public static void main(String[] args) {
         LinkedListOperation list = new LinkedListOperation();
-        list.push(70);
-        list.push(30);
         list.push(56);
+        list.push(30);
+        list.push(70);
         list.printList();
     }
 }
