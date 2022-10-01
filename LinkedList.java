d6e899702f6549905987e1bbdad37e0f3a21dc17
Ashutosh <ashu.cse.sln@gmail.com>
2022-10-01 08:25:58 +0530

    UC3 completd

--- LinkedList.java
+++ LinkedList.java
@@ -3,9 +3,10 @@ package org.example;
 public class LinkedList {
     public static void main(String[] args) {
         LinkedListOperation list = new LinkedListOperation();
-        list.push(70);
-        list.push(30);
-        list.push(56);
+        list.append(56);
+        list.append(30);
+        list.append(70);
         list.printList();
+
     }
 }
--- LinkedListOperation.java
+++ LinkedListOperation.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class LinkedListOperation {
     Node head;
+
     public class Node {
 
         int data;
@@ -13,18 +14,33 @@ public class LinkedListOperation {
 
         }
     }
+
     /* Inserts a new Node at front of the list. */
     public void push(int new_data) {
         Node new_node = new Node(new_data);
         new_node.next = head;
         head = new_node;
     }
-    public void printList()
-    {
+
+    /* Appends a new node at the end.  This method is
+       defined inside LinkedList class shown above */
+    public void append(int new_data) {
+        Node new_node = new Node(new_data);
+        if (head == null) {
+            head = new Node(new_data);
+            return;
+        }
+        new_node.next = null;
+        Node last = head;
+        while (last.next != null)
+            last = last.next;
+        last.next = new_node;
+    }
+
+    public void printList() {
         Node tempNode = head;
-        while (tempNode != null)
-        {
-            System.out.print(" -> "+ tempNode.data);
+        while (tempNode != null) {
+            System.out.print(" -> " + tempNode.data);
             tempNode = tempNode.next;
         }
     }
