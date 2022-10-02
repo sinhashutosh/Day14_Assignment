eee7aa7139243d5cd766456e141b22cbd38de141
Ashutosh <ashu.cse.sln@gmail.com>
2022-10-02 13:08:06 +0530

    Queue using linked list

--- LinkedList.java
+++ LinkedList.java
@@ -2,20 +2,10 @@ package org.example;
 
 public class LinkedList {
     public static void main(String[] args) {
-        StackUsingLinkedlist obj = new StackUsingLinkedlist();
-        obj.push(70);
-        obj.push(30);
-        obj.push(56);
-        obj.display();
-        System.out.println("\npeek element = " + obj.peek());
-        obj.pop();
-        obj.display();
-        System.out.println("\npeek element = " + obj.peek());
-        obj.pop();
-        obj.display();
-        System.out.println("\npeek element = " + obj.peek());
-        obj.pop();
-        System.out.println("\npeek element = " + obj.peek());
-        obj.display();
+        QueueUsingLinkedlist q = new QueueUsingLinkedlist();
+        q.enqueue(56);
+        q.enqueue(30);
+        q.enqueue(70);
+        q.printQueue();
     }
 }
---
+++ QueueUsingLinkedlist.java
@@ -0,0 +1,48 @@
+package org.example;
+
+public class QueueUsingLinkedlist {
+    private Node front, rear;
+    private int currentSize;
+
+    private class Node {
+        int data;
+        Node next;
+    }
+
+
+    public QueueUsingLinkedlist() {
+        front = null;
+        rear = null;
+        currentSize = 0;
+    }
+
+    public boolean isEmpty() {
+        return (currentSize == 0);
+    }
+
+    public void enqueue(int data) {
+        Node oldRear = rear;
+        rear = new Node();
+        rear.data = data;
+        rear.next = null;
+        if (isEmpty()) {
+            front = rear;
+        } else {
+            oldRear.next = rear;
+        }
+        currentSize++;
+        System.out.println(data + " added to the queue");
+    }
+    public void printQueue() {
+        Node temp = front;
+        System.out.println("Linked List :");
+
+        while(temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+}
+
